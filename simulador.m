21cf6d2dd0943a75e6217be54ba105b43215262b
Carolina Barcelo <carolinabarcelooliver@hotmail.com>
2016-04-27 19:24:49 +0200

    subida inicial del simulador

---
+++ simulador.m
@@ -0,0 +1,14 @@
+function [salidaSimulador] = simulador(eventos, nHilos, Qmax)
+    % nHilos == N
+    % Qmax
+    % Suponemos que el video está codificado para una velocidad de datos C y que dura un minuto en ser enviado a velocidad C.
+    [f,c]=size(eventos);
+    for i=1:f
+        peticion = eventos(i,:);
+        tipo = peticion(1);
+        tllegada = peticion(2);
+        idllegada = peticion(3);
+        tservicio = peticion(4);
+        
+    end
+end
