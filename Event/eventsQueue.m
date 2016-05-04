083c5f568080c8f32e7224bea539e908c50a5fe7
Chris <chris@quickapps.es>
2016-05-04 18:09:09 +0200

    refactor

--- Event/eventsQueue.m
+++ Event/eventsQueue.m
@@ -4,13 +4,32 @@ classdef eventsQueue < handle
     end
 
     methods
+        % Inicializa la cola.
+        %
+        % # Entradas:
+        %
+        % {q}: Un listado de eventos resultado de alguno de los generadores
         function [] = setQueue(obj, q)
             obj.queue = q;
         end;
         
+        % Extrae y retorna el primer elemento de la cola
+        %
+        % # Retorno:
+        %
+        % Un evento de la cola 
         function [e] = pop(obj)
             e = obj.queue(1);
             obj.queue(1) = [];
+        end;
+        
+        % Verifica si la cola esta vacía o no.
+        %
+        % # Retorno:
+        %
+        % 1 si la cola está vacia, 0 en otro caso.
+        function [e] = isEmpty(obj)
+            e = isempty(obj.queue);
         end;
     end
 end
