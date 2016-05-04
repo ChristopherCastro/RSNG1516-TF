de50e949b1778d6ae07ead44cdaf7f40a3a4dfe6
iñigo ezcurdia <inigoezcurdia@gmail.com>
2016-05-04 19:05:58 +0200

    Cleaned main.m

--- main.m
+++ main.m
@@ -2,9 +2,5 @@ addpath('Event/');
 addpath('Event/Generator');
 addpath('Simulator');
 
-<<<<<<< HEAD
-events = generador_2(0.2, 5, 1000, 254984519);
-=======
 events = generador_1(0.2, 5, 1000, 254984519);
-queue = eventsQueue(events);
->>>>>>> 3c979ffc9d4768604fd9fe6473935318a50d6944
+
