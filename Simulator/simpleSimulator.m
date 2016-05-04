18e3497f289fc5fe2253f115f41baed0e92fb384
Carolina Barcelo <carolinabarcelooliver@hotmail.com>
2016-05-04 19:55:43 +0200

    simulador + lio de colas

--- Simulator/simpleSimulator.m
+++ Simulator/simpleSimulator.m
@@ -1,31 +1,42 @@
 % Simulador básico con un único servidor y múltiples hilos.
 %
-function [salidaSimulador] = simpleSimulator(nHilos, Qmax, eventos)
+function [salidaSimulador] = simpleSimulator(nHilos, Qmax, lQueue)
     % nHilos == N
     % Qmax
     % Suponemos que el video está codificado para una velocidad de datos C y que dura un minuto en ser enviado a velocidad C.
     N = nHilos;
-    nmax=length(eventos);
-    qEventos = [];
-    qClientes = []; % cola de clientes atenidos por el servidor
-    salidaSimulador = zeros(7,nmax);
-
-
-    for i=1:nmax
-        
-        if (strcmp(eventos(i).tipo, 'L'))
-            %meter en la cola
-            if length(qClientes)<N
-                eventos(i).tipo = 'S';
-                % modificar tiempos de lledada y de servicio
-
-                qClientes(end+1) = eventos(i);
+    nmax=length(lQueue);
+    sQueue = eventsQueue();
+    
+    while(~lQuenue.isEmpty() || ~sQuenue.isEmpty())
+        e = getEvent(lQueue, sQueue);
+        if(e.tipo == 'L')
+            if (sQuenue.count() < nHilos)
+                sQuenue.
+                sQuenue.push(e);
             end
-        elseif (strcmp(eventos(i).tipo, 'S'))
-            evento = qClientes(1);
-            qClientes(1)=[];
-            evento.
+        else
         end
-        
     end
+
+   
+end
+
+function e = getEvent(lQueue, sQueue)
+    l = struct('tllegada', inf);
+    s = l;    
+    
+    if(~lQuenue.isEmpty() )
+        l = lQuenue.first();
+    end
+    if(~sQuenue.isEmpty() )
+        s = lQuenue.first();
+    end
+    
+    if (l.tllegada < s.tllegada)
+        e = lQueue.pop();
+        
+    else
+        e = sQueue.pop();
+    end    
 end
