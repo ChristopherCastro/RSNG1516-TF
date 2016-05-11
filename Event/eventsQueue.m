4eb7db7be37c9475eedd5a19de6b8a581275638e
Chris <chris@quickapps.es>
2016-05-11 17:54:25 +0200

    updated queue manager

--- Event/eventsQueue.m
+++ Event/eventsQueue.m
@@ -1,6 +1,18 @@
+% A simple and generic queue manager.
+%
+% # Usage:
+%
+% ```matlab
+%  queue = new eventsQueue(listOfEvents);
+%  while queue.hasNext()
+%      event = queue.next();
+%      % do nifty things with {event}
+%  end;
+% ```
 classdef eventsQueue < handle
     properties
         queue = [];
+        head = 1;
     end
 
     methods
@@ -12,35 +24,46 @@ classdef eventsQueue < handle
             end;
         end;
         
-        % Extrae y retorna el primer elemento de la cola
+        % Establece la cabeza de la cola a una posición arbitraria.
+        %
+        % - {newHead}: Nueva posición de la cabeza.
+        function [] = setHead(obj, newHead)
+            obj.head = newHead;
+        end;
+        
+        % Retorna el elemento apuntado por la cabeza.
         %
         % # Retorno:
         %
         % Un evento de la cola 
-        function [e] = pop(obj)
-            e = obj.queue(1);
-            obj.queue(1) = [];
+        function [e] = next(obj)
+            e = obj.queue(obj.head);
+            obj.head = obj.head + 1;
         end;
         
-        % Retorna el número de elementos en la cola.
+        % Retorna el número de elementos en la cola. Es decir, el número de
+        % elementos entre el último elemento extraido y el final de la
+        % cola en sí.
         %
         % # Retorno
         %
         % Un entero mayor o igual a cero
         function [c] = count(obj)
-            c = length(obj.queue);
+            c = length(obj.queue) - obj.head - 1;
         end;
 
-        % Verifica si la cola esta vacía o no.
+        % Verifica si hay más elementos en la cola o no.
         %
         % # Retorno:
         %
-        % 1 si la cola está vacia, 0 en otro caso.
-        function [e] = isEmpty(obj)
-            e = isempty(obj.queue);
+        % - 1: Si la cola NO está vacia.
+        % - 0: En otro caso.
+        function [e] = hasNext(obj)
+            e = obj.head <= length(obj.queue);
         end;
 
-        % Consula el primer elemento de la cola sin quitarlo de la misma.
+        % Consula el primer elemento de la cola sin quitarlo ni avanzar la
+        % cabezade la misma.
         %
         % # Retorno:
         %
@@ -49,7 +72,8 @@ classdef eventsQueue < handle
             f = obj.queue(1);
         end;
         
-        % Consula el último elemento de la cola sin quitarlo de la misma.
+        % Consula el último elemento de la cola sin quitarlo ni alterar la
+        % cabeza de la misma.
         %
         % # Retorno:
         %
@@ -62,8 +86,8 @@ classdef eventsQueue < handle
         %
         % # Argumentos
         %
-        % {event}: Un nuevo evento
-        function push(obj, event)
+        % - {event}: Un nuevo evento
+        function add(obj, event)
             obj.queue(end + 1) = event;
         end;
     end
