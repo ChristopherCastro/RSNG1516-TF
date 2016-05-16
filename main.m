c6278f6feb6e6db96baba90f72f70aa68cf2e785
Chris <chris@quickapps.es>
2016-05-16 18:46:10 +0200

    fix

--- main.m
+++ main.m
@@ -5,8 +5,3 @@ addpath('Simulator');
 events = generador_1(0.2, 50, 2000, 254984519);
 eventsQ = eventsQueue(events);
 [salida] = simpleSimulator(2, 2, eventsQ);
-
-fprintf('Salida thread1: %d elementos.\n', size(salida.queues{1}.queue,2));
-fprintf('Salida thread2: %d elementos.\n', size(salida.queues{2}.queue,2));
-fprintf('Rechazadas: %d elementos.\n', rechazadas);
-fprintf('Total: %d elementos.\n', size(salida.queues{1}.queue,2)+size(salida.queues{2}.queue,2)+rechazadas);
