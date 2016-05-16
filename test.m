f7290d66548ba8e3ca39aa0b8164ba695c40b479
iñigo ezcurdia <inigoezcurdia@gmail.com>
2016-05-16 17:48:23 +0200

    Probando sizes firsts y next de nuestras colas raras

--- test.m
+++ test.m
@@ -2,4 +2,20 @@ addpath('Event/');
 addpath('Event/Generator');
 addpath('Simulator');
 
-disp('Este sistema de testeo apesta!');
+
+
+colarara = eventsQueue([1 2 3 4 5 6 7 8 9]);
+
+fprintf('Tamaño inicial: %d\n',colarara.size());
+
+fprintf('first(): %d\n',colarara.first());
+
+fprintf('Tamaño tras first: %d\n',colarara.size());
+
+fprintf('next(): %d\n',colarara.next());
+
+fprintf('Tamaño tras next: %d\n',colarara.size());
+
+fprintf('next(): %d\n',colarara.next());
+
+fprintf('Tamaño tras next: %d\n',colarara.size());
