efe335296d4e37ff865d159e8eff3046b1397395
Chris <chris@quickapps.es>
2016-05-16 17:55:37 +0200

    revert main

--- main.m
+++ main.m
@@ -4,7 +4,4 @@ addpath('Simulator');
 
 events = generador_1(0.2, 50, 2000, 254984519);
 eventsQ = eventsQueue(events);
-[salida, rechazadas] = simpleSimulator(2, 2, eventsQ);
-
-disp(salida);
-disp(rechazadas);
+[salida] = simpleSimulator(2, 2, eventsQ);
