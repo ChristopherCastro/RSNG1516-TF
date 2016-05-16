734c4b29e91d7bc40053491cbe050a693f2dba0c
Carolina Barcelo <carolinabarcelooliver@hotmail.com>
2016-05-16 17:49:24 +0200

    seguimiento rechazadas

--- main.m
+++ main.m
@@ -4,6 +4,7 @@ addpath('Simulator');
 
 events = generador_1(0.2, 50, 2000, 254984519);
 eventsQ = eventsQueue(events);
-salida = simpleSimulator(2, 2, eventsQ);
+[salida, rechazadas] = simpleSimulator(2, 2, eventsQ);
 
 disp(salida);
+disp(rechazadas);
