5b8273ffa78cab5b217ea3835d37e88eca87fe73
Chris <chris@quickapps.es>
2016-05-16 19:33:37 +0200

    update

--- main.m
+++ main.m
@@ -2,6 +2,6 @@ addpath('Event/');
 addpath('Event/Generator');
 addpath('Simulator');
 
-events = generador_1(0.2, 10, 10000, 254984519);
+events = generador_1(0.2, 50, 10000, 254984519);
 eventsQ = eventsQueue(events);
-[salida] = simpleSimulator(2, 50, eventsQ);
+[salida] = simpleSimulator(3, 10, eventsQ);
