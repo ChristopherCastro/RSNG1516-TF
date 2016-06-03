985232ff2f4f3bf3a7bb74c76e520cefcce87bab
Chris <chris@quickapps.es>
2016-06-03 20:36:54 +0200

    fix erlangB

--- Stats/erlang.m
+++ Stats/erlang.m
@@ -1,6 +1,18 @@
+% Grafica de Erlang-B, que indica la probabilidad de ser rechazado en
+% función del numero de clientes en un instante dado y el número hilos de
+% procesamiento disponibles.
+%
+% Esta función calcula múltiples funciones de probabilidad para distintas
+% configuraciones de número de hilos. De forma que conocida la
+% configuración de un sistema (su número de hilos) se pueda comparar con
+% otras configuraciones alternativas con el objetivo de encontraar un valor
+% de número de hilos adecuado / óptimo al problema.
+%
+% nThreads: Número de hilos, recomendable que sea un multiple de 5.
+% rho: Factor de utilización máxima.
 function [] = erlang(nThreads, rho)
-    syms ro k i;
-    erlangB(ro, k) = (ro^k / factorial(k)) / symsum(ro^i / factorial(i), i, [0 k]);
+    syms ro k ii;
+    erlangB(ro, k) = (ro^k / factorial(k)) / symsum(ro^ii / factorial(ii), ii, [0 k]);
 
     nThreads = 0:5:(nThreads + mod(nThreads, 5));
     legends = {};
